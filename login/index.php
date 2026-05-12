0854af9da0b0b5a461d4423e4a3da90f0395042f
Emanuele D'Andrea <emanueledandrea@proton.me>
2026-05-12 13:31:42 +0200

    feat: add login and register pages with form controls

---
+++ login/index.php
@@ -0,0 +1,78 @@
+<!DOCTYPE html>
+<html lang="en" data-bs-theme="dark">
+
+<head>
+  <meta charset="UTF-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <title>Login | Dummy CRUD</title>
+  <link rel="stylesheet" href="../main.css">
+
+  <!-- Bootstrap icons -->
+  <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap-icons@1.13.1/font/bootstrap-icons.min.css">
+
+  <script>
+    // Theme adapt to system
+    function setTheme(isDark) {
+      document.documentElement.dataset.bsTheme = isDark ? 'dark' : 'light';
+    }
+
+    const isDark = window.matchMedia('(prefers-color-scheme: dark)');
+    isDark.addEventListener('change', (e) => setTheme(e.matches));
+    setTheme(isDark.matches);
+
+  </script>
+
+  <!-- Bootstrap CSS minified -->
+  <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.8/dist/css/bootstrap.min.css" rel="stylesheet"
+    integrity="sha384-sRIl4kxILFvY47J16cr9ZwB07vP4J8+LH7qKQnuqkuIAvNWLzeN8tE5YBujZqJLB" crossorigin="anonymous">
+
+</head>
+
+<body>
+   <?php include __DIR__ . '/../includes/header.php'; ?>
+
+  <main>
+    <div class="width padding">
+      <form action="">
+        <h2>Log-in</h2>
+        <div class="mb-3">
+          <label for="username" class="form-label">Username</label>
+          <input type="text" class="form-control" id="username" name="username">
+          <div class="invalid-feedback">
+            Invalid username.
+          </div>
+        </div>
+        <div class="mb-3">
+          <label for="password" class="form-label">Password</label>
+          <div class="input-group">
+            <input type="password" class="form-control" id="password" name="password">
+            <div class="invalid-feedback">
+              Invalid password.
+            </div>
+            <button class="btn btn-outline-secondary" type="button" id="" title="Display password"><i
+                class="bi bi-eye-slash-fill"></i></button>
+          </div>
+        </div>
+        <div class="col-12 d-flex justify-content-center mb-3">
+          <button class="btn btn-primary" type="submit">Login <i class="bi bi-box-arrow-in-right"></i></button>
+        </div>
+        <a href="../register/" title="Go to register">Don't have an account?</a>
+      </form>
+    </div>
+  </main>
+
+  <?php include __DIR__ . '/../includes/footer.php'; ?>
+
+  <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.8/dist/js/bootstrap.bundle.min.js"
+    integrity="sha384-FKyoEForCGlyvwx9Hj09JcYn3nv7wiPVlz7YYwJrWVcXK/BmnVDxM+D2scQbITxI" crossorigin="anonymous">
+    </script>
+
+  <script>
+    // Popper tooltips
+    const tooltipTriggerList = document.querySelectorAll('[title]')
+    const tooltipList = [...tooltipTriggerList].map(tooltipTriggerEl => new bootstrap.Tooltip(tooltipTriggerEl))
+  </script>
+
+</body>
+
+</html>
